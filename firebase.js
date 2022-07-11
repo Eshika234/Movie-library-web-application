ead6d1abf4d2a455da673303361fc15c60fd11cf
Eshika Singha <98522796+Eshika234@users.noreply.github.com>
2022-07-11 23:46:26 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,16 @@
+var firebaseConfig = {
+
+    apiKey: "AIzaSyA02VhgGjKafAQCf39ZJm1l8zo9MFQGnYw",
+    authDomain: "authentication-app-f13a6.firebaseapp.com",
+    databaseURL: "https://authentication-app-f13a6-default-rtdb.firebaseio.com",
+    projectId: "authentication-app-f13a6",
+    storageBucket: "authentication-app-f13a6.appspot.com",
+    messagingSenderId: "187557755359",
+    appId: "1:187557755359:web:69bfa479e11ffcfbfdec6a",
+    measurementId: "G-L8JBL9F1MG"
+  };
+  
+
+   // Initialize Firebase
+   firebase.initializeApp(firebaseConfig);
+   firebase.analytics();
